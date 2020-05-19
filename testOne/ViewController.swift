c75eb3aca45578b5261f2dbfbe5884b21c5b10dc
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-05-18 23:57:44 -0400

    1

--- testOne/ViewController.swift
+++ testOne/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    @IBOutlet var showTextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showTextButtonPressed() {
+    }
+    
+    
 }
 
